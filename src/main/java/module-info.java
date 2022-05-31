70fb2d2adc92604d6569f7a2e9e899b15a2a2656
mafallon94 <106597704+mafallon94@users.noreply.github.com>
2022-05-31 14:33:54 +0100

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module fsd.assignment.assignment1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens fsd.assignment.assignment1 to javafx.fxml;
-    exports fsd.assignment.assignment1;
-}
